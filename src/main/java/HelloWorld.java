7bd22ce2bd0b2759918f8cb3474b3209c9502a0b
indrapalsingh <indra_singh@persistent.com>
2018-01-23 10:09:42 +0530

    Git Maven Project

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,12 @@
 
-public class Main {
+public class HelloWorld {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Maven");
+		System.out.print("text");
 
 	}
 
